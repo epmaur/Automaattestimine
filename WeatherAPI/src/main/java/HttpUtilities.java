94a1903a0b7be20f283f534222aae5440a58d005
Epp <epmaur@ttu.ee>
2017-09-24 22:34:34 +0300

    test response status code

---
+++ WeatherAPI/src/main/java/HttpUtilities.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+/**
+ * Created by Epu on 24.09.2017.
+ */
+public class HttpUtilities {
+
+    public static int getResponseStatusCode() {
+        Integer [] statusCodes = new Integer[] {200, 404, 418};
+        Random generator = new Random();
+        int randomArrayIndex = generator.nextInt(statusCodes.length);
+        return statusCodes[randomArrayIndex];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getResponseStatusCode());
+    }
+}
---
+++ WeatherAPI/src/test/java/HttpUtilitiesTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Epu on 24.09.2017.
+ */
+public class HttpUtilitiesTest {
+
+
+    @Before
+    public void initObjects() {
+    }
+
+    @Test
+    public void testIfResponseStatusCodeIsOK() {
+        int responseStatusCodeOK = HttpUtilities.getResponseStatusCode();
+        assertEquals(responseStatusCodeOK, 200);
+    }
+
+}
